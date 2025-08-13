b35ccc5264ed555eab894c4c5f743cff54f51203
Jorge <ingjorgelopezxx@gmail.com>
2025-08-12 22:01:59 -0400

    actualizar2  módulo sabiasque y actualización de main.py

--- components/sabiasque.py
+++ components/sabiasque.py
@@ -8,10 +8,25 @@ SABIASQUE_ITEMS = [
         "titulo": "¿Sabías que las cucarachas pueden vivir hasta una semana sin su cabeza?",
         "imagen": "https://www.gardentech.com/-/media/project/oneweb/gardentech/images/pest-id/bug-pest/cockroach.png",
         "texto": (
-            "Tienen un sistema nervioso que les permite sobrevivir un tiempo sin ella, "
-            "aunque finalmente mueren por falta de agua. Además, pueden aguantar hasta "
-            "40 minutos sin respirar y soportar niveles de radiación que matarían a un humano. "
+            "Las cucarachas son plagas muy resistentes y adaptables, con diversas curiosidades que las hacen fascinantes y problemáticas. "
+            "Son capaces de sobrevivir sin cabeza por un tiempo, soportan altos niveles de radiación y pueden sobrevivir sin comida por semanas o sin agua por días. "
+            "Además, pueden transmitir enfermedades y son atraídas por la comida y la humedad, lo que las convierte en una amenaza para la salud y la higiene."
+            "Algunas especies de cucarachas pueden vivir hasta un mes sin comida, adaptándose fácilmente a diferentes entornos, "
+            "pueden correr hasta 3 millas por hora, lo que las hace muy difíciles de atrapar. Su velocidad y agilidad les permiten escapar " 
+            "rápidamente de los depredadores y de los intentos de captura. Además, son capaces de girar rápidamente en ángulos agudos y esconderse en espacios reducidos. " 
+            "esta capacidad de supervivencia las convierte en una de las plagas más resistentes y difíciles de eliminar. "
         ),
+        "extra": [
+            "Resistencia: Pueden sobrevivir sin cabeza por hasta una semana y pueden aguantar la respiración hasta por 40 minutos.",
+            "Adaptabilidad: Se adaptan a diversos ambientes, pero prefieren interiores cálidos y húmedos, cerca de fuentes de alimento y agua.",
+            "Transmisión de enfermedades: Transmiten salmonela, E. coli y otras bacterias peligrosas.",
+            "Comportamiento: Son nocturnas, se esconden de la luz y atraen a otras mediante rastros químicos.",
+            "Reproducción: Se multiplican rápidamente, generando infestaciones graves.",
+            "Alimentación: Comen desde alimentos humanos y de mascotas hasta basura y materia orgánica.",
+            "Impacto en la salud: Pueden causar alergias y asma, sobre todo en niños.",
+            "No solo la cucaracha americana: También hay otras plagas como la oriental y la alemana.",
+            "Evitar pisarlas: Al aplastarlas pueden dispersar bacterias y gérmenes peligrosos."
+        ]
     },
     {
         "titulo": "¿Sabías que una sola rata puede producir hasta 2,000 descendientes en un año si no se controla su población?",
@@ -20,6 +35,14 @@ SABIASQUE_ITEMS = [
             "Las ratas tienen una gran capacidad reproductiva y, sin medidas de control, "
             "pueden convertirse rápidamente en una infestación grave. Además de los daños "
             "materiales, son portadoras de enfermedades peligrosas. ¡Por eso el control de plagas es esencial!"
+        ),
+    },
+    {
+        "titulo": "¿Sabías que las termitas nunca duermen?",
+        "imagen": "https://i.postimg.cc/zD0Rq1dr/termitas-1.png",
+        "texto": (
+            "Estas pequeñas plagas trabajan sin descanso, día y noche, comiendo madera y debilitando estructuras. "
+            "Su actividad constante puede causar daños graves a edificios y muebles si no se controla a tiempo. ¡Protege tu hogar con un control de plagas profesional y mantén a las termitas lejos!"
         ),
     },
 ]
@@ -92,6 +115,27 @@ def render_sabiasque(page: ft.Page, contenedor: ft.Column, items: list | None = 
                         color=ft.Colors.BLACK,
                         text_align=ft.TextAlign.JUSTIFY,
                     ),
+                    ft.Column(
+                        controls=[
+                            ft.Row(
+                    [
+                     ft.Text("•", size=18,color=ft.Colors.BLACK),
+                                ft.Text(
+                                    p.split(":", 1)[0] + ":",
+                                    size=14,color=ft.Colors.BLACK,
+                                    weight=ft.FontWeight.BOLD
+                                ),
+                                ft.Text(
+                                    p.split(":", 1)[1].strip() if ":" in p else "",
+                                    size=14,color=ft.Colors.BLACK
+                                )
+                            ],
+                            spacing=6
+                        )
+                        for p in d.get("extra", [])
+                    ],
+                    spacing=2
+                ) if "extra" in d else ft.Container()
                 ],
                 spacing=10,
                 expand=True,
