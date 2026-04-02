c47313c5d8fb6f25fc0e81b7259efbc0880443a0
Jorge <ingjorgelopezxx@gmail.com>
2026-04-02 10:12:49 -0300

    Actualizando Visual de la pagina2

--- functions/runtime.py
+++ functions/runtime.py
@@ -1,5 +1,4 @@
 import os
-
 import flet as ft
 
 from functions.flet_compat import apply_flet_compat
@@ -8,12 +7,13 @@ from functions.flet_compat import apply_flet_compat
 def run_web_app() -> None:
     apply_flet_compat()
 
-    app_view = getattr(ft, "AppView", None)
-    web_view = app_view.WEB_BROWSER if app_view else getattr(ft, "WEB_BROWSER", None)
     from processes.web_app import main as web_main
 
-    ft.run(
-        web_main,
+    app_view = getattr(ft, "AppView", None)
+    web_view = getattr(app_view, "WEB_BROWSER", None) if app_view else getattr(ft, "WEB_BROWSER", None)
+
+    ft.app(
+        target=web_main,
         view=web_view,
         port=int(os.environ.get("PORT", 8080)),
         assets_dir="assets",
